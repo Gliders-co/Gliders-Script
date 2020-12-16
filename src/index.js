aec0b04667a0964ce2b9fcc1be3be6157c4f1790
niranjank151 <63844648+niranjank151@users.noreply.github.com>
2020-12-16 14:30:46 +0530

    Updated the script

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import Gliders from './Fetch';
+
+ export const GlidersScript = { init : async function(componentId, apiKey) {
+
+    let data = await Gliders(componentId,apiKey);
+ 
+    const div = document.getElementById ("gliders-component");
+    //const div = document.createElement('div');
+        const node=    document.createRange()
+        .createContextualFragment(data)
+
+        div.insertBefore(node, div.firstChild);
+  
+    return div;
+
+
+  }
+}
+  
+  document.body.appendChild(component(data));
+  
